c007726454a1f8895d62b96fdfd1c0e781d01177
mybezci <mybezci@gmail.com>
2022-11-28 00:17:21 +0100

    inital login 1

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -13,6 +13,7 @@ export class LoginComponent implements OnInit {
   constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.createLoginForm();
   }
 
   createLoginForm(){
